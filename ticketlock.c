484719be90fd65bb316f518ad496920c4e1558fb
MohammadMahdi <mahdialvansaz@gmail.com>
2020-01-24 07:53:06 +0330

    initTicketlock is done

--- ticketlock.c
+++ ticketlock.c
@@ -1,4 +1,116 @@
 //
 // Created by mohammadmahdi on 1/22/20.
 //
+// Mutual exclusion spin locks.
+
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "x86.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "spinlock.h"
+#include "ticketlock.h"
+
+void
+initTicketlock(struct ticketlock *lk, char *name)
+{
+    lk->name = name;
+    lk->proc = 0;
+    lk->ticket = 0;
+    lk->turn = 0;
+}
+
+// Acquire the lock.
+void
+acquireTicketlock(struct spinlock *lk)
+{
+    uint ticket;
+    if(holdingTicketlock(lk))
+        panic("acquire");
+    ticket = fetch_and_add(&lk->ticket, 1);
+    while (lk->turn != ticket)
+        give
+}
+
+// Release the lock.
+void
+release(struct spinlock *lk)
+{
+    if(!holding(lk))
+        panic("release");
+
+    lk->pcs[0] = 0;
+    lk->cpu = 0;
+
+    // Tell the C compiler and the processor to not move loads or stores
+    // past this point, to ensure that all the stores in the critical
+    // section are visible to other cores before the lock is released.
+    // Both the C compiler and the hardware may re-order loads and
+    // stores; __sync_synchronize() tells them both not to.
+    __sync_synchronize();
+
+    // Release the lock, equivalent to lk->locked = 0.
+    // This code can't use a C assignment, since it might
+    // not be atomic. A real OS would use C atomics here.
+    asm volatile("movl $0, %0" : "+m" (lk->locked) : );
+
+    popcli();
+}
+
+// Record the current call stack in pcs[] by following the %ebp chain.
+void
+getcallerpcs(void *v, uint pcs[])
+{
+    uint *ebp;
+    int i;
+
+    ebp = (uint*)v - 2;
+    for(i = 0; i < 10; i++){
+        if(ebp == 0 || ebp < (uint*)KERNBASE || ebp == (uint*)0xffffffff)
+            break;
+        pcs[i] = ebp[1];     // saved %eip
+        ebp = (uint*)ebp[0]; // saved %ebp
+    }
+    for(; i < 10; i++)
+        pcs[i] = 0;
+}
+
+// Check whether this cpu is holding the lock.
+int
+holdingTicketlock(struct ticketlock *lock)
+{
+    return (lock->ticket != lock->turn ) && (lock->proc == proc);
+}
+
+
+// Pushcli/popcli are like cli/sti except that they are matched:
+// it takes two popcli to undo two pushcli.  Also, if interrupts
+// are off, then pushcli, popcli leaves them off.
+
+void
+pushcli(void)
+{
+    int eflags;
+
+    eflags = readeflags();
+    cli();
+    if(mycpu()->ncli == 0)
+        mycpu()->intena = eflags & FL_IF;
+    mycpu()->ncli += 1;
+}
+
+void
+popcli(void)
+{
+    if(readeflags()&FL_IF)
+        panic("popcli - interruptible");
+    if(--mycpu()->ncli < 0)
+        panic("popcli");
+    if(mycpu()->ncli == 0 && mycpu()->intena)
+        sti();
+}
+
+
 
